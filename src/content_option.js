4a00352c1ef29e6ee5cf0ffcacf274badb4ed3c1
Ashish acharya G <113185898+theashish05@users.noreply.github.com>
2023-06-12 14:45:37 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,161 @@
+const logotext = "AA";
+const meta = {
+    title: "Ashish Acharya",
+    description: "I’m Ashish Acharya ,React developer,currently working in Bengaluru",
+};
+
+const introdata = {
+    title: "I’m Ashish Acharya",
+    animated: {
+        first: "I code cool websites",
+        second: "I love coding",
+        third: "I design templates,cards",
+    },
+    description: "To leverage my expertise in React development and passion for creating intuitive user interfaces to contribute to the success of a dynamic and innovative organization, while continuously expanding my skill set and staying up-to-date with the latest industry trends and technologies.",
+    your_img_url: "https://media.licdn.com/dms/image/D5603AQEnstYIMCI8DA/profile-displayphoto-shrink_200_200/0/1672547675458?e=1691625600&v=beta&t=2GcS-jm2sYUZW-KwnraJ3Ybx7snlcBOyan8pBkmGhro",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "I am a highly skilled and motivated React developer with a passion for crafting exceptional user experiences. With [number] years of experience in front-end web development, I have honed my expertise in React.js and its ecosystem, including Redux, Hooks, and React Router. My strong understanding of JavaScript, HTML, and CSS enables me to create dynamic and visually appealing interfaces.",
+};
+const worktimeline = [
+    {
+        jobtitle: "React Developer",
+        where: "Mobily Infotech",
+        date: "2023",
+    },
+];
+
+const skills = [
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "HTML",
+        value: 85,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "Redux",
+        value: 80,
+    },
+    {
+        name: "Core Java",
+        value: 80,
+    },
+];
+
+const services = [{
+    title: "Web Application Development:",
+    description: " Utilize your expertise in React.js to develop high-quality, scalable, and interactive web applications tailored to clients' specific needs. This includes front-end development, UI/UX design implementation, and integrating with APIs and back-end systems.",
+},
+{
+    title: "Responsive Web Design",
+    description: " Build web applications that are optimized for different devices and screen sizes, ensuring a consistent user experience across desktops, tablets, and mobile devices.",
+},
+{
+    title: "Integration of Third-Party Libraries and APIs",
+    description: "  Integrate external libraries, frameworks, or APIs into React applications to extend functionality and improve user workflows. This can include payment gateways, social media integrations, mapping services, and more.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://media.licdn.com/dms/image/D5603AQEnstYIMCI8DA/profile-displayphoto-shrink_200_200/0/1672547675458?e=1691625600&v=beta&t=2GcS-jm2sYUZW-KwnraJ3Ybx7snlcBOyan8pBkmGhro",
+    description: "Resume",
+    link:"https://drive.google.com/file/d/1ATyYaIgJgoQGXuxspDuVQ4dkBx3z0y7q/view?usp=sharing",
+},
+{
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUGKvBoGgsPxDELkcZf3XPFsuzpSGWnjU9Z9A-yDcM3g&usqp=CAU&ec=48665699",
+    description: "AM Shopify",
+    link: "https://am-shopify.netlify.app/",
+},
+{
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI1-o0VU7FB2adKZWqu7NvNQZrI2qJFX2rdXkhb8iI7g&usqp=CAU&ec=48665699",
+    description: "Cards Templates",
+    link: "Contact",
+},
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ashishacharyag305@gmail.com",
+    YOUR_FONE: "9880043832",
+    description: "Feel free to contact me at [Your email address] or [Your phone number]. I'm available for new opportunities and would love to discuss how I can contribute to your project.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_8dqbsic",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/theashish05",
+    facebook: "https://facebook.com/ashishacharyag",
+    linkedin: "https://www.linkedin.com/in/ashish-acharya-g-946109220",
+    instagram: "https://instagram.com/_the_ashish",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB4pyFzCS2X-ubzcRMix4B4NbDyrOm35bY17H585eklw&usqp=CAU&ec=48665699",
+//     description: "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+// },
+// {
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZHvQZvB6J2fv0IW5QiKBNNmAIY1t1TGV1N-VBNPsSaw&usqp=CAU&ec=48665699",
+//     description: "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+// },
+// {
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh5gK8XC4hiA1G1o_D99z3ji6E51-WlB-fuqIgKx9kBg&usqp=CAU&ec=48665699",
+//     description: "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+// },
+
+// {
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs8LGcCpKau75OpsxqgcXVMhecYIlq0Ap9YxRn-PzLVw&usqp=CAU&ec=48665699",
+//     description: "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+// },
+// {
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWz4Ib4fnw6t4as8eoDiYaEafylI-_At0j9Dm1ubUc4KUBsdarwGEGp1r9CtyKcTFIgacL2yDIRM&usqp=CAU&ec=48665699",
+//     description: "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+// },
+// {
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZyT1_HNB4aKbSe0AYxUvp7nL7x6nGHVxLQ7WDx84O7A&usqp=CAU&ec=48665699",
+//     description: "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+// }
